c896c15b10e211a29ef40a9b63bddb179530fd4f
Davis Zong <93000346+davis5040@users.noreply.github.com>
2022-01-17 15:19:14 +0000

    I solved problem B.

--- Asampleread.py
+++ Asampleread.py
@@ -1,3 +1,10 @@
 """
 Write a program (doesn't have to be a function, but can be) that reads what's in sample.txt and prints it to the console.
 """
+fstream = open("sample.txt", 'r')
+line = fstream.readline()
+while line:
+  print(line);
+  line = fstream.readline()
+
+fstream.close()
--- Baminoacids.py
+++ Baminoacids.py
@@ -5,4 +5,25 @@ weightedstring should read from masstable.txt. It's helpful to have those masses
 """
 
 def weightedstring(protein):
-  pass
+  fstream = open("masstable.txt", 'r')
+  contents = fstream.readlines()
+  dic = {}
+  d = 0
+  cur = 'A'
+  for line in contents:
+    for c in line.split():
+      #print(c)
+      #print(d)
+      #print(cur)
+      if d == 1:
+        dic[cur] = c
+        d = 0
+      else:
+        cur = c
+        d = 1
+  ans = 0
+  for x in protein:
+    k = dic[x]
+    intk = float(k)
+    ans = ans + intk
+  print(ans)
